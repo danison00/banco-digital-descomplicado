1734acec2571ff04bf567f96079885451b7f0808
danison <danison.santos00@gmail.com>
2023-09-12 19:45:24 -0300

    user repository update

--- banco-digital-descomplicado/src/main/java/com/dan/bancodigitaldescomplicado/repository/UserRepository.java
+++ banco-digital-descomplicado/src/main/java/com/dan/bancodigitaldescomplicado/repository/UserRepository.java
@@ -1,9 +1,14 @@
 package com.dan.bancodigitaldescomplicado.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.dan.bancodigitaldescomplicado.model.entity.User;
 
+
 public interface UserRepository extends JpaRepository<User, Long>{
+
+    Optional<User> findByUsername(String username);
     
 }
