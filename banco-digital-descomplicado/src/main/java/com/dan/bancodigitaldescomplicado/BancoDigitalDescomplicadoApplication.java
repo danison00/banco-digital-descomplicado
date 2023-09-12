61b9e104379f0fe0198083881c87a73d74e8ab2d
danison <danison.santos00@gmail.com>
2023-09-12 09:47:33 -0300

    init project

---
+++ banco-digital-descomplicado/src/main/java/com/dan/bancodigitaldescomplicado/BancoDigitalDescomplicadoApplication.java
@@ -0,0 +1,13 @@
+package com.dan.bancodigitaldescomplicado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoDigitalDescomplicadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoDigitalDescomplicadoApplication.class, args);
+	}
+
+}
---
+++ banco-digital-descomplicado/src/test/java/com/dan/bancodigitaldescomplicado/BancoDigitalDescomplicadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dan.bancodigitaldescomplicado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoDigitalDescomplicadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
