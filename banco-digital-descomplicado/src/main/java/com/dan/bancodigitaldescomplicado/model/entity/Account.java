1adee40e65b66a35624f4419b6ec0035615a520c
danison <danison.santos00@gmail.com>
2023-09-12 11:12:17 -0300

    created account

---
+++ banco-digital-descomplicado/src/main/java/com/dan/bancodigitaldescomplicado/model/entity/Account.java
@@ -0,0 +1,34 @@
+package com.dan.bancodigitaldescomplicado.model.entity;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+import jakarta.persistence.*;
+import lombok.Data;
+
+
+@Data
+@Entity
+@Table(name = "Accounts")
+public class Account implements Serializable{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String number;
+    private BigDecimal balance;
+    private TypeAccount type;
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate openingDate;
+
+    @OneToMany(mappedBy = "origin")
+    private List<Transaction> transactionsSend;
+    
+    @OneToMany(mappedBy = "destination")
+    private List<Transaction> transactionsReceived;
+
+}
