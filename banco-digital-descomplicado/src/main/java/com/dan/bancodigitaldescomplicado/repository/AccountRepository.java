59ea5dac0c03a2c66322e4ed9513408a4e09448e
danison <danison.santos00@gmail.com>
2023-09-12 23:27:18 -0300

    repository update

--- banco-digital-descomplicado/src/main/java/com/dan/bancodigitaldescomplicado/repository/AccountRepository.java
+++ banco-digital-descomplicado/src/main/java/com/dan/bancodigitaldescomplicado/repository/AccountRepository.java
@@ -4,6 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.dan.bancodigitaldescomplicado.model.entity.Account;
 
+import java.util.Optional;
+
+
 public interface AccountRepository extends JpaRepository<Account, Long>{
+
+    Optional<Account> findByNumber(String number);
     
 }
--- banco-digital-descomplicado/src/main/java/com/dan/bancodigitaldescomplicado/repository/UserRepository.java
+++ banco-digital-descomplicado/src/main/java/com/dan/bancodigitaldescomplicado/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.dan.bancodigitaldescomplicado.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.dan.bancodigitaldescomplicado.model.entity.User;
 
@@ -10,5 +11,8 @@ import com.dan.bancodigitaldescomplicado.model.entity.User;
 public interface UserRepository extends JpaRepository<User, Long>{
 
     Optional<User> findByUsername(String username);
+
+    @Query("SELECT u FROM User u WHERE u.conta.number = :accountNumber")
+    Optional<User> findByAccountNumber(String accountNumber);
     
 }
