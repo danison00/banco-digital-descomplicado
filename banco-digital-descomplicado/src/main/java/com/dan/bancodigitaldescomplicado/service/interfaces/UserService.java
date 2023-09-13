25dbb9ca36fcd4b175467f1d2dd3d32fac2cbe19
danison <danison.santos00@gmail.com>
2023-09-12 23:26:54 -0300

    service update

--- banco-digital-descomplicado/src/main/java/com/dan/bancodigitaldescomplicado/service/interfaces/UserService.java
+++ banco-digital-descomplicado/src/main/java/com/dan/bancodigitaldescomplicado/service/interfaces/UserService.java
@@ -5,8 +5,11 @@ import java.util.Optional;
 import com.dan.bancodigitaldescomplicado.model.entity.User;
 
 public interface UserService {
-    
+
     Optional<User> findByUsername(String username) throws Exception;
+
     User save(User user) throws Exception;
+
+    User findByAccountNumber(String accountNumber) throws Exception;
 
 }
