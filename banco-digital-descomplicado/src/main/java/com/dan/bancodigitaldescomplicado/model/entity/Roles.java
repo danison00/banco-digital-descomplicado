fb5e72f111584e0d0ca3e0905f3decc1f5e86c01
danison <danison.santos00@gmail.com>
2023-10-01 11:06:21 -0300

    Roles created

---
+++ banco-digital-descomplicado/src/main/java/com/dan/bancodigitaldescomplicado/model/entity/Roles.java
@@ -0,0 +1,19 @@
+package com.dan.bancodigitaldescomplicado.model.entity;
+
+public enum Roles {
+
+    ADMIN("admin"), USER("user");
+
+    private String role;
+
+    
+    private Roles(String role) {
+        this.role = role;
+    }
+
+
+    public String getRole() {
+        return role;
+    }
+    
+}
