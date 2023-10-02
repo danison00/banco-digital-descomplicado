0db217f20f6a617f5e7416fb7d7f99ee3662a26e
danison <danison.santos00@gmail.com>
2023-10-02 11:21:36 -0300

    security config update

--- banco-digital-descomplicado/src/main/java/com/dan/bancodigitaldescomplicado/infra/webSecurity/SecurityConfig.java
+++ banco-digital-descomplicado/src/main/java/com/dan/bancodigitaldescomplicado/infra/webSecurity/SecurityConfig.java
@@ -1,23 +1,57 @@
 package com.dan.bancodigitaldescomplicado.infra.webSecurity;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.dan.bancodigitaldescomplicado.model.entity.Roles;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    
+
+    @Autowired
+    private SecurityFilter securityFilter;
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
         return http.csrf(csrf -> csrf.disable())
-        .authorizeHttpRequests(authorize -> authorize.requestMatchers("/**").permitAll().anyRequest().authenticated())
-        .build();
-        
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.DELETE, "/account").hasRole("USER")
+                        .requestMatchers(HttpMethod.POST, "/account").permitAll()
+                        .requestMatchers("/transaction/deposit").permitAll()
+                        .requestMatchers("/transaction/transfer").hasRole("USER")
+                        .requestMatchers("/user/**").permitAll()
+                        .anyRequest().authenticated())
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+
+        return authenticationConfiguration.getAuthenticationManager();
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
