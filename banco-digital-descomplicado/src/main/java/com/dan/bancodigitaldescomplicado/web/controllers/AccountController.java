da08c4429aa5e6cd09666d272770a3f67787b871
danison <danison.santos00@gmail.com>
2023-10-02 20:53:24 -0300

    update mapper

--- banco-digital-descomplicado/src/main/java/com/dan/bancodigitaldescomplicado/web/controllers/AccountController.java
+++ banco-digital-descomplicado/src/main/java/com/dan/bancodigitaldescomplicado/web/controllers/AccountController.java
@@ -3,12 +3,10 @@ package com.dan.bancodigitaldescomplicado.web.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.dan.bancodigitaldescomplicado.model.dto.AccountResponseDto;
-import com.dan.bancodigitaldescomplicado.model.entity.User;
 import com.dan.bancodigitaldescomplicado.service.interfaces.AccountService;
 import com.dan.bancodigitaldescomplicado.util.Mapper;
 
@@ -28,7 +26,7 @@ public class AccountController {
         String username = authentication.getPrincipal().toString();
 
         System.out.println(username);
-        AccountResponseDto accountDto = mapper.fromAccountToAccountResponseDto(accountService.getDataAccount(username));
+        AccountResponseDto accountDto = mapper.fromAccountToAccountResponseDto(accountService.findByUsername(username));
 
         return ResponseEntity.ok().body(accountDto);
     }
